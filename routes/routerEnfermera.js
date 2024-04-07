15fd9d5a7f7e4b9bdf7846a96aa9dd7ea66ae521
Alexxomby <72172506+Alexxomby@users.noreply.github.com>
2024-04-06 23:53:31 -0600

    Claro esta que se deben de ver las rutas para que funcione
    Pero ese sera trabajo de otro pringado que no sea yo

--- routes/routerEnfermera.js
+++ routes/routerEnfermera.js
@@ -1,4 +1,28 @@
 const express = require('express')
 const routerE = express.Router()
 
+//Registro
+routerE.get('/Registrarme_como_enfermera', (req, res)=>{
+    res.render('RegisterE', {alert:false})
+})
+//Login
+routerE.get('/Iniciar_sesion_enfermera', (req, res)=>{
+    res.render('LoginE', {alert:false})
+})
+//Vacantes
+routerE.get('/Visualizar_vacantes', (req, res)=>{
+    res.render('Vacantes', {alert:false})
+})
+//Info Vacante
+routerE.get('/Informacion_detallada_vacante', (req, res)=>{
+    res.render('InfoVacanteE',)
+})
+//sin Verificar
+routerE.get('/', (req, res)=>{
+    res.render('ViewwithoutVerify', {alert:false})
+})
+//Su Informacion
+routerE.get('/Informacion_personal' , (req, res)=>{
+    res.render('InfoE', )
+})
 module.exports = routerE
