510eefd9325f4e1ce558b9caa3a7f664a025db67
Alexxomby <72172506+Alexxomby@users.noreply.github.com>
2024-04-19 21:56:58 -0600

    Ya quedo

--- controllers_Acces/authEnfermera.js
+++ controllers_Acces/authEnfermera.js
@@ -115,6 +115,9 @@ exports.IniciarSesionEnfermeras = async (req, res) => {
         const correo = req.body.correoe;
         const contrasena = req.body.passe;
 
+        let passHashe = await bcryptjs.hash(contrasena, 8) 
+
+
         console.log('Este es iniciarsesion 2'+ correo + contrasena)
 
         // Consultar el usuario en la base de datos
@@ -162,14 +165,14 @@ exports.IniciarSesionEnfermeras = async (req, res) => {
         });
 
         // Redirigir al usuario a una página de inicio o dashboard después de iniciar sesión
-        res.render('Visualizar_vacantes', {
+        res.render('./Enfermera/VacantesE', {
             alert: true,
             alertTitle: "Conexión exitosa",
             alertMessage: "¡LOGIN CORRECTO!",
             alertIcon: 'success',
             showConfirmButton: false,
             timer: 800,
-            ruta: './Enfermera/VacantesE'
+            ruta: 'Visualizar_vacantes'
         });
 
     } catch (error) {
@@ -242,3 +245,70 @@ exports.ObtenerInfo = async (req, res, next) => {
 exports.DocsVerify = async (req, res, next) => {
     console.log('entrando a la verificacion de los documentos ')
 }
+
+
+
+exports.IniciarSesionUsuario2 = async (req, res) => {
+    try {
+        const user = req.body.user;
+        const pass = req.body.pass;
+        console.log('Este es iniciarsesion')
+
+        // Consultar el usuario en la base de datos
+        const results = await queryAsync('SELECT idDatosA, CorreoA, PassA FROM datosa WHERE CorreoA = ?', [user]);
+        console.log(results)
+
+        if (results.length === 0 || !await bcryptjs.compare(pass, results[0].PassA)) {
+            console.log(results)
+            // Si no se encuentra un usuario con las credenciales proporcionadas o la contraseña no coincide, retornar un mensaje de error
+            return res.render('login2', {
+                alert: true,
+                alertTitle: "Error",
+                alertMessage: "Usuario y/o contraseña incorrectos",
+                alertIcon: 'error',
+                showConfirmButton: true,
+                timer: 1000,
+                ruta: 'Logindos'
+            });
+        }
+
+        // Obtener información del usuario para incluir en el token JWT
+        const userId = results[0].idDatosA;
+        const userEmail = results[0].CorreoA;
+
+        const resultadosUser = await queryAsync('SELECT idDatosG, NombreG FROM datosg WHERE idDatosA = ?', [userId]);
+
+
+        const Id_usuario = resultadosUser[0].idDatosG;
+        const nom_usuario = resultadosUser[0].NombreG;
+
+
+        // Generar el token JWT con más información del usuario
+        const token = jwt.sign({ idDatosG: Id_usuario, NombreG: nom_usuario, idDatosA:userId }, process.env.JWT_SECRETO, {
+            //expiresIn: process.env.JWT_COOKIE_EXPIRES
+        });
+        console.log(token+' tokensin')
+
+        // Enviar el token JWT al cliente
+        res.cookie('jwt', token, {
+           // expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
+            httpOnly: true
+        
+        });
+
+        // Redirigir al usuario a una página de inicio o dashboard después de iniciar sesión
+        res.render('vista_usuario', {
+            alert: true,
+            alertTitle: "Conexión exitosa",
+            alertMessage: "¡LOGIN CORRECTO!",
+            alertIcon: 'success',
+            showConfirmButton: false,
+            timer: 800,
+            ruta: 'loginBien'
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Hubo un error al iniciar sesión' });
+    }
+};
