737b61e2c23dcbc901c3837e6e9af7d7bfaed427
Alexxomby <avazquezs1902@alumno.ipn.mx>
2024-05-17 21:58:09 -0600

    nosewporque pero funciona el desplazamiento

---
+++ CitasController/crearVacante.js
@@ -0,0 +1,24 @@
+/**
+ * Pues aqui pondremos los valores para que puedamos crear una vacante
+ * ocupamos
+ * id_direccion
+ * id_usuario
+ * id_usuario Enfermera
+ * id_pac
+ * id_sol
+ * id_horario
+ * id_dh
+ * 
+ * la tabla tiene 
+ * id_sol` INT NOT NULL AUTO_INCREMENT,
+  `des_sol` VARCHAR(500) NULL,
+  `tipo_sol` ENUM('cuid', 'enfe') NULL,
+  `est_sol` ENUM('Espera', 'Curso', 'Final') ,
+  `cost_sol` DECIMAL(7,2) NULL,
+  `id_hor` INT NOT NULL,
+  `id_us` INT NOT NULL,
+  `id_pac` INT NOT NULL,
+  `id_emp` INT NULL,
+  `id_dir`
+ * 
+ */
