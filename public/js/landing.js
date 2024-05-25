00fe71a4ceba27cfcea5c2afdd8ee07587e962e9
Alexxomby <avazquezs1902@alumno.ipn.mx>
2024-05-24 18:59:59 -0600

    Revert "Squashed commit of the following:"
    This reverts commit 845b5a26ad46208f36c074ce9d7e9adf5ae6e4cf.

--- public/js/landing.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').addClass('active');
-    });
-
-    $('#dismiss').on('click', function () {
-        $('#sidebar').removeClass('active');
-    });
-});
