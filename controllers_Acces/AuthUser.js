cf3a8d0bc5129ab8097f26a4358cfa68f708a3fe
Alexxomby <avazquezs1902@alumno.ipn.mx>
2024-06-01 22:29:11 -0600

    Lado bueno, verifioca quer el correo este registrado

--- controllers_Acces/AuthUser.js
+++ controllers_Acces/AuthUser.js
@@ -9,17 +9,7 @@ const query = promisify(conexionU.query).bind(conexionU);
 // Convierte la función query en una función que devuelve una promesa
 const queryAsync = promisify(conexionU.query).bind(conexionU);
 
-//procedimiento para registrarnos
 
-/**metodo para agregar direccion del usuario 
-/**metodo para mostrar las direcciones del usuario 
-/**metpodo para registrar paciente 
-/**metodo para crear chamba 
-
-
-y todos los metodos correspondientes para editar estos mismos datos
-
-*/
 
 exports.IniciarSesionUsuario = async (req, res) => {
     try {
@@ -128,20 +118,6 @@ exports.logout = (req, res)=>{
     return res.redirect('/')
 }
 
-/**
- * que pasa si yo quiero consumir algo en un lenguje de programacion diferente
- * si necesito que esa app se conecte en postGress??
- * necesitamos un servidor que se encargue de recibir las peticiones y enviar las respuestas
- * y eso se llama API REST , son traductores de un codigo a otro
- * el traductor se llama JSON, le envias un JSON y te regresa un JSON 
- * mandamos una lista de datos y nos regresa una lista de datos
- * lo que hacemos en mantenimiento es, actualizaciones
- * 
- * cambiando de tema para nuestro soporte
- * el minimo de levantar ticket, gestionar ticket
- * asignarlo, darle seguimineot y de acuerdo 
- * al nuestra politica de soporte si se puede marcar como resuelto, en curso etc
- */
 
 
 /**
@@ -172,17 +148,16 @@ exports.crearUsuario = async (req, res) => {
         }
         let passHashe = await bcryptjs.hash(passw, 8)
 
-        let dataAcces = await query("INSERT INTO datos_acceso (cor_datacc,pas_datacc, rol_datacc) VALUES (?,?,'cliente')", [correo,passHashe]);
+        let dataAcces = await query("INSERT INTO datos_acceso (cor_datacc,pas_datacc, rol_datacc) VALUES (?,?,'clie')", [correo,passHashe]);
 
         let idDataAcces = dataAcces.insertId;
 
         
 
         //incercion de los datos en la base de datos
-        let dataDireccion = await query("INSERT INTO direccion (del_dir, col_dir, calle_dir, cp_dir) VALUES (?,?,?,?)",[alcaldia, colonia, calle, codigo_postal])
-        let idDataDirec = dataDireccion.insertId;
+        
 
-        let U = await query("INSERT INTO user (nom_us, pat_us, mat_us, fot_us, tel_us, id_datacc, id_dir) VALUES (?,?,?,'N/A',?,?,?)", [nombre, apellido_paterno, apellido_materno, numero_telefono, idDataAcces, idDataDirec]);
+        let U = await query("INSERT INTO user (nom_us, pat_us, mat_us, fot_us, tel_us, id_datacc) VALUES (?,?,?,'N/A',?,?)", [nombre, apellido_paterno, apellido_materno, numero_telefono, idDataAcces]);
         let iduser = U.insertId;
         console.log("exito")
 
